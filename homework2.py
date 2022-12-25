e94939410f2ce2078f70961ebaf4e46efd8e614b
Sektant88 <120678054+Sektant88@users.noreply.github.com>
2022-12-25 15:34:52 +0200

    Update homework2.py

--- homework2.py
+++ homework2.py
@@ -1,12 +1,26 @@
 a=123
 b=123
-print(id(a))
-print(id(b))
+c=123
+d=[2,3,4,5]
+e=[2,3,4,5]
 print(a==b)
+print(a==c)
+print(b==c)
+print(d==e)
 print(a is b)
+print(a is c)
+print(b is c)
+print(d is e)
 a=float(a)
 b=float(b)
-print(id(a))
-print(id(b))
+c=float(c)
+d=str(d)
+e=str(d)
 print(a==b)
+print(a==c)
+print(b==c)
+print(d==e)
 print(a is b)
+print(a is c)
+print(b is c)
+print(d is e)
