e34004b4e022da735e06c835fb4f68ae9a5f4483
Sektant88 <120678054+Sektant88@users.noreply.github.com>
2023-01-07 05:12:35 +0200

    Update homewrok10.py

--- homewrok10.py
+++ homewrok10.py
@@ -3,17 +3,21 @@ def func():
     try_1=0
     secret=random.randint(0,15)
     while try_1 < 5:
-        number=int(input('Введите число: '))
-        if number < secret:
+        number=input('Введите число: ')
+        if number.isdigit():
+            number = int(number)
+        else:
+            print('ошибка,это не число,заново!')
+        if number > secret:
              print('число меньше загаданного')
-        elif number > secret:
+        elif number < secret:
             print('число больше загаданного')
         else:
             print(f"вы угадали число {secret}")
             break
         try_1+=1
     else:
-        print(f"число {secret} так и не было угадано")
+        print(f"ха-ха,не угадали это число: {secret} ")
 func()
 
 
