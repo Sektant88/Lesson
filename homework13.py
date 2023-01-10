21cafe5b417cda1c5197d9fd41cbd81d62ad8e3c
Sektant88 <120678054+Sektant88@users.noreply.github.com>
2023-01-10 23:18:50 +0200

    Add files via upload

---
+++ homework13.py
@@ -0,0 +1,29 @@
+def func():
+    a=int(input('введите число: '))
+    if a % 2 ==0:
+        print('четное')
+    else:
+        print('не четное')
+
+def func_2():
+    result = 0
+    number = int(input("Число: "))
+    while number > 0:
+        if number % 3 != 0:
+            result += number ** 3
+        number -= 1
+    print(result)
+
+def decorator():
+    def wrapper():
+        from datetime import datetime
+        time = datetime.now()
+        func()
+        durationTimed_1 = datetime.now() - time
+        print('время 1-ой ф-кции: ',durationTimed_1)
+        func_2()
+        durationTimed_2 = datetime.now() - time
+        print('время 2-ой ф-кции: ',durationTimed_2)
+    return wrapper()
+decorator()
+
