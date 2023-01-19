ed12a6d951622cf7cec50fa624a6cf3656c1b847
Sektant88 <120678054+Sektant88@users.noreply.github.com>
2023-01-19 22:22:58 +0200

    Update homewrok15.py

--- homewrok15.py
+++ homewrok15.py
@@ -2,9 +2,9 @@ start= b'r\xc3\xa9sum\xc3\xa9'
 print(start)
 second=start.decode()
 print(second)
-Thirh=second.encode('Latin1')
+Thirh=second.encode('Latin1','ignore')
 print(Thirh)
-d=Thirh.decode('utf-8','ignore')
+d=Thirh.decode('Latin1')
 print(d)
 
 
