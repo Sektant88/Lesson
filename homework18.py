65751eb20b6517fc945a2b15921e7bc37d8c8a8b
Sektant88 <120678054+Sektant88@users.noreply.github.com>
2023-01-24 17:27:22 +0200

    Update homework18.py

--- homework18.py
+++ homework18.py
@@ -4,26 +4,29 @@ class auto(object):
     mark:str
     weight:int
     color:str
-    def __init__(self,brand:str,age:int,mark:str):
+    def __init__(self,brand:str,age:int,mark:str,color:str,weight:int):
         self.brand=brand
         self.age=age
         self.mark=mark
-
+        self.color=color
+        self.weight=weight
+    
     def move(self):
-        self.move='move'
-        print(self.move)
-    def stop(self):
-        self.stop='stop'
-        print(self.stop)
-    def birthday(self):
-        self.birthday=self.age+1
-        print('birthday: ',self.birthday)
+        print('move')
 
-camry=auto("toyota",2012,"camry")
-print(camry.brand+" "+camry.mark+' '+camry.age.__str__())
+    def stop(self):
+        print('stop')
+
+    def birthday(self):
+        self.age+=1
+
+camry=auto("toyota",2012,"camry","black",4)
+print(camry.brand+" "+camry.mark+' '+' '+'color:',camry.color+', '+'weight: ',camry.weight.__str__())
+print('age:',camry.age)
+camry.birthday()
+print('birthday:',camry.age)
 camry.move()
 camry.stop()
-camry.birthday()
 
 
 
