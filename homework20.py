23b73bd5a2df61b52474e817ae3e22d49f8b3567
Sektant88 <120678054+Sektant88@users.noreply.github.com>
2023-01-28 22:14:36 +0200

    Add files via upload

---
+++ homework20.py
@@ -0,0 +1,11 @@
+import json
+old_dict={
+    567890:('danya',20),
+    347890:('Lisa',20),
+    76525678:('Anna',18),
+    342167:('Anna',30),
+    568903:('Ira',25)
+}
+with open('2.json','w') as f:
+    json.dump(old_dict,f)
+
