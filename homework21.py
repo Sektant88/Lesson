918288316701ec20bd508df98f2880406ee01d0a
Sektant88 <120678054+Sektant88@users.noreply.github.com>
2023-01-30 01:12:51 +0200

    Update homework21.py

--- homework21.py
+++ homework21.py
@@ -2,11 +2,14 @@ import json
 with open('2.json','r') as f:
     output_data = json.load(f)
 import csv
+
+import random
 new=[]
 for item in output_data:
+    num = str(random.randint(0, 999999999))
     value=output_data[item]
+    value.append(num)
     new.append(value)
-    value.append(380973450346)
 print(new)
 with open('3.csv','w') as w_file:
     file_writter=csv.writer(w_file)
