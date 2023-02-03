bb3eb610ff7a7b07f07eb4ddc5e97fb9db010229
Sektant88 <120678054+Sektant88@users.noreply.github.com>
2023-02-03 15:22:44 +0200

    Update homework23.py

--- homework23.py
+++ homework23.py
@@ -6,31 +6,37 @@ class String(str):
 
     def __add__(self, other):
         other = str(other)
+        print(self.Stroka+other)
         return String(self.Stroka+other)
-    
+
     def __sub__(self, other):
         other = str(other)
+        print(String(self.Stroka.replace(other, '', 1)))
         return String(self.Stroka.replace(other, '', 1))
 
-print(String('New') + String(890))
-print(String(1234) + 5678)
-print(String('New') + 'castle')
-print(String('New') + 77 )
-print(String('New') + True)
-print(String('New') + ['s', ' ', 23])
-print(String('New') + None)
-print(type(String('New') + None))
+String('New') + String(890)
+String(1234) + 5678
+String('New') + 'castle'
+String('New') + True
+String('New') + ['s', ' ', 23]
+String('New') + None
+print((type(String('New') + None)))
 
 
 
-print(String('New bala7nce') - 7)
-print(String('New balance') - 'bal')
-print(String('New balance') - 'Bal')
-print(String('pineapple apple pine') - 'apple')
-print(String('New balance') - 'apple')
-print(String('NoneType') - None)
-print(String(55678345672) - 7)
+String('New bala7nce') - 7
+String('New balance') - 'bal'
+String('New balance') - 'Bal'
+String('pineapple apple pine') - 'apple'
+String('New balance') - 'apple'
+String('NoneType') - None
+String(55678345672) - 7
 print(type(String('NoneType') - None))
+
+
+
+
+
 
 
 
