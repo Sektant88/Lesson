6d3198038c3530c971d8f672fc4854218dad7018
Sektant88 <120678054+Sektant88@users.noreply.github.com>
2023-02-04 13:11:29 +0200

    Add files via upload

---
+++ homework24.py
@@ -0,0 +1,42 @@
+class Myclass():
+    a:int
+
+    def __init__(self,a:int):
+        self.a=a
+
+
+    def __add__(self, other):
+        return self.a+other.a
+
+    def __truediv__(self, other):
+      return self.a / other.a
+
+    @staticmethod
+    def zorro(d):
+        return d.a * 2
+
+    @staticmethod
+    def sanji(b):
+        return b.a**2
+
+
+
+
+obj=Myclass(10)
+fjd=Myclass(20)
+
+a=obj.zorro(obj)
+print(a)
+
+b=obj.sanji(obj)
+print(b)
+
+plus=obj+fjd
+print(plus)
+
+gg=fjd / obj
+print(gg)
+
+
+
+
